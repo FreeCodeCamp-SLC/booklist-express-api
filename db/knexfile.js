1f6d592564384bd599bdd1a0d1af4283a12dd56f
Alex Puhl <alexpuhldeveloper@gmail.com>
2021-03-12 08:35:31 -0700

    Built out the start of hitting auth0 endpoints

---
+++ db/knexfile.js
@@ -0,0 +1,83 @@
+const { DB_CLIENT, DB_HOST, DB_USERNAME, DB_DATABASE, DB_TEST_DATABASE, DB_PASSWORD, DB_PORT } = require('../config');
+
+module.exports = {
+	test: {
+		client: DB_CLIENT,
+		connection: {
+			host: DB_HOST,
+			database: DB_TEST_DATABASE,
+			port: DB_PORT,
+			user: DB_USERNAME,
+			password: DB_PASSWORD
+		},
+		migrations: {
+			directory: __dirname + '/migrations'
+		},
+		seeds: {
+			directory: __dirname + '/seeds/test'
+		},
+		log: {
+			warn(message) {
+			},
+			error(message) {
+			},
+			deprecate(message) {
+			},
+			debug(message) {
+			},
+		}
+	},
+	development: {
+		client: DB_CLIENT,
+		connection: {
+			host: DB_HOST,
+			database: DB_DATABASE,
+			port: DB_PORT,
+			user: DB_USERNAME,
+			password: DB_PASSWORD
+		},
+		migrations: {
+			directory: __dirname + '/migrations'
+		},
+		seeds: {
+			directory: __dirname + '/seeds/development'
+		}
+	},
+	staging: {
+		client: DB_CLIENT,
+		connection: {
+			host: DB_HOST,
+			database: DB_DATABASE,
+			port: DB_PORT,
+			user: DB_USERNAME,
+			password: DB_PASSWORD
+		},
+		pool: {
+			min: 2,
+			max: 20
+		},
+		migrations: {
+			tableName: 'knex_migrations'
+		}
+	},
+	production: {
+		client: DB_CLIENT,
+		connection: {
+			host: DB_HOST,
+			database: DB_DATABASE,
+			port: DB_PORT,
+			user: DB_USERNAME,
+			password: DB_PASSWORD
+		},
+		pool: {
+			min: 2,
+			max: 10
+		},
+		migrations: {
+			directory: __dirname + '/migrations'
+		},
+		seeds: {
+			directory: __dirname + '/seeds/production'
+		}
+	}
+};
--- index.js
+++ index.js
@@ -1,13 +1,113 @@
-require('dotenv').config();
+'use strict';
+
 const express = require('express');
+const morgan = require('morgan');
+const moment = require('moment-timezone');
+const cors = require('cors');
+var jwt = require('express-jwt');
+var jwks = require('jwks-rsa');
+const jwtAuthz = require('express-jwt-authz');
+const jwksRsa = require('jwks-rsa');
+
+const routes = require('./routes');
+
+const { SERVER_PORT, CLIENT_ORIGIN, NODE_ENV } = require('./config');
 
 const app = express();
 
-const port = process.env.PORT;
+morgan.token('date', (req, res, tz) => moment()
+  .tz(tz)
+  .format('h:mm:ss a')
+);
+
+app.use(
+	morgan(
+		'":method :url" :status :res[content-length] - :response-time ms [:date[America/Denver]]',
+	)
+);
+
+app.use(
+	cors({
+		origin: CLIENT_ORIGIN,
+	}),
+);
 
 app.use(express.json());
 
-app.get('/', (req, res) => res.json({ message: 'Hello World' }));
-app.listen(process.env.PORT, () =>
-  console.log(`Example app listening on port ${process.env.PORT}!`)
-);
+// app.use(routes); 
+
+const checkJwt = jwt({
+  secret: jwks.expressJwtSecret({
+      cache: true,
+      rateLimit: true,
+      jwksRequestsPerMinute: 5,
+      jwksUri: 'https://utahfcc.us.auth0.com/.well-known/jwks.json'
+}),
+audience: 'https://booklist/api',
+issuer: 'https://utahfcc.us.auth0.com/',
+algorithms: ['RS256']
+});
+
+
+// This route doesn't need authentication
+app.get('/api/public', function(req, res) {
+  res.json({
+    message: 'Hello from a public endpoint! You don\'t need to be authenticated to see this.'
+  });
+});
+
+// ----------BEGIN AUTH0 STUFF---------------
+// app.use(checkJwt);
+
+// This route needs authentication
+app.get('/api/private', checkJwt, function(req, res) {
+  res.json({
+    message: 'Hello from a private endpoint! You need to be authenticated to see this.'
+  });
+});
+
+const checkScopes = jwtAuthz([ 'read:books' ]);
+
+app.get('/api/private-scoped', checkJwt, checkScopes, function(req, res) {
+  res.json({
+    message: 'Hello from a private endpoint! You need to be authenticated and have a scope of read:books to see this.'
+  });
+});
+
+app.get('/authorized', function (req, res) {
+  res.send('Secured Resource');
+});
+// ----------END AUTH0 STUFF---------------
+
+app.use((req, res, next) => {
+	const err = new Error('Not Found');
+	err.status = 404;
+	next(err);
+});
+
+app.use((err, req, res, next) => {
+	if (err.status) {
+		const errBody = Object.assign({}, err, { message: err.message });
+		res.status(err.status).json(errBody);
+	} else {
+		res.status(500).json({ message: 'Internal Server Error' });
+		if (err.name !== 'FakeError') console.log(err);
+	}
+});
+
+function runServer(port = SERVER_PORT) {
+	const server = app
+		.listen(port, () => {
+			console.info(`App listening on port ${server.address().port}`);
+		})
+		.on('error', (err) => {
+			console.error('Express failed to start');
+			console.error(err);
+		});
+}
+
+if (require.main === module) {
+	runServer();
+}
+
+module.exports = app;
