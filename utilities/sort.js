4828712cf49e829fbdbebaccbdb600577beb962a
Justin Howard <50159035+zorro1rr@users.noreply.github.com>
2022-08-18 15:18:09 -0600

    fixed year sorting bug

--- utilities/sort.js
+++ utilities/sort.js
@@ -1,48 +1,26 @@
 function sort(sortOption, lists) {
-  console.log('sortOption', sortOption);
-  console.log('lists', lists);
   let sortBy;
   switch (sortOption) {
     case 'Name: Asc':
-      if (lists) {
-        sortBy = 'NAME ASC';
-      } else {
-        sortBy = 'TITLE ASC';
-      }
+      sortBy = lists ? 'NAME ASC' : 'TITLE ASC';
       break;
     case 'Name: Desc':
-      if (lists) {
-        sortBy = 'NAME DESC';
-      } else {
-        sortBy = 'TITLE DESC';
-      }
+      sortBy = lists ? 'NAME DESC' : 'TITLE DESC';
       break;
     case 'Most Recent: Asc':
-      if (lists) {
-        sortBy = 'LIST_ID ASC';
-      } else {
-        sortBy = 'BOOK_ID ASC';
-      }
+      sortBy = lists ? 'LIST_ID ASC' : 'BOOK_ID ASC';
       break;
     case 'Most Recent: Desc':
-      if (lists) {
-        sortBy = 'LIST_ID DESC';
-      } else {
-        sortBy = 'BOOK_ID DESC';
-      }
+      sortBy = lists ? 'LIST_ID DESC' : 'BOOK_ID DESC';
       break;
     case 'Year: Asc':
-      sortBy = 'YEAR ASC';
+      sortBy = lists ? 'YEAR ASC' : 'TITLE ASC';
       break;
     case 'Year: Desc':
-      sortBy = 'YEAR DESC';
+      sortBy = lists ? 'YEAR DESC' : 'TITLE ASC';
       break;
     default:
-      if (lists) {
-        sortBy = 'NAME ASC';
-      } else {
-        sortBy = 'TITLE ASC';
-      }
+      sortBy = lists ? 'NAME ASC' : 'TITLE ASC';
   }
   return sortBy;
 }
