3693578efb4da8c79009ab0dce19df7d99c43b6d
Justin Howard <50159035+zorro1rr@users.noreply.github.com>
2023-02-05 21:57:12 -0700

    testing

--- db/pg.js
+++ db/pg.js
@@ -2,8 +2,15 @@ const { Pool } = require('pg');
 
 const environment = process.env.NODE_ENV || 'development';
 
-const { connection } = require('./pgDbConnections')[environment];
+// const { connection } = require('./pgDbConnections')[environment];
 
-const pool = new Pool(connection);
+// const pool = new Pool(connection);
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 module.exports = pool;
