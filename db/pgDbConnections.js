318b4cf82ae8b2300f61110fa20ec87a6bafc0e9
Justin Howard <50159035+zorro1rr@users.noreply.github.com>
2023-02-08 22:03:23 -0700

    test99

--- db/pgDbConnections.js
+++ db/pgDbConnections.js
@@ -1,42 +1,42 @@
-const {
-  DB_HOST, DB_USERNAME, DB_DATABASE, DB_TEST_DATABASE, DB_PASSWORD, DB_PORT, PROD_DB_PASSWORD,
-} = require('../config');
+// const {
+//   DB_HOST, DB_USERNAME, DB_DATABASE, DB_TEST_DATABASE, DB_PASSWORD, DB_PORT, PROD_DB_PASSWORD,
+// } = require('../config');
 
-module.exports = {
-  test: {
-    connection: {
-      host: DB_HOST,
-      database: DB_TEST_DATABASE,
-      port: DB_PORT,
-      user: DB_USERNAME,
-      password: DB_PASSWORD,
-    },
-  },
-  development: {
-    connection: {
-      host: DB_HOST,
-      database: DB_DATABASE,
-      port: DB_PORT,
-      user: DB_USERNAME,
-      password: DB_PASSWORD,
-    },
-  },
-  staging: {
-    connection: {
-      host: DB_HOST,
-      database: DB_DATABASE,
-      port: DB_PORT,
-      user: DB_USERNAME,
-      password: DB_PASSWORD,
-    },
-  },
-  production: {
-    connection: {
-      host: DB_HOST,
-      database: DB_DATABASE,
-      port: DB_PORT,
-      user: DB_USERNAME,
-      password: PROD_DB_PASSWORD,
-    },
-  },
-};
+// module.exports = {
+//   test: {
+//     connection: {
+//       host: DB_HOST,
+//       database: DB_TEST_DATABASE,
+//       port: DB_PORT,
+//       user: DB_USERNAME,
+//       password: DB_PASSWORD,
+//     },
+//   },
+//   development: {
+//     connection: {
+//       host: DB_HOST,
+//       database: DB_DATABASE,
+//       port: DB_PORT,
+//       user: DB_USERNAME,
+//       password: DB_PASSWORD,
+//     },
+//   },
+//   staging: {
+//     connection: {
+//       host: DB_HOST,
+//       database: DB_DATABASE,
+//       port: DB_PORT,
+//       user: DB_USERNAME,
+//       password: DB_PASSWORD,
+//     },
+//   },
+//   production: {
+//     connection: {
+//       host: DB_HOST,
+//       database: DB_DATABASE,
+//       port: DB_PORT,
+//       user: DB_USERNAME,
+//       password: PROD_DB_PASSWORD,
+//     },
+//   },
+// };
